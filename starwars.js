fe3eb473e9f9e6b32145d606c519e064b8ac5ada
Braden <fish0859@gmail.com>
2025-02-27 17:19:16 -0700

    Starships and Pilots now printing out in a organized mannor

--- starwars.js
+++ starwars.js
@@ -1,12 +1,10 @@
 const starships = [];
 const pilots = {};
-// let url_page = 0;
-// let req_fail = 0;
 
 async function fetch_starships(url = "https://swapi.dev/api/starships") {
     try  {
         // url_page++
-        console.log(`fetching ${url}`)
+        // console.log(`fetching ${url}`)
         //* attempt to fetch next paginated url before prior fetch
         // await fetch_starships(url+`/?page=${url_page}`)
 
@@ -25,37 +23,48 @@ async function fetch_starships(url = "https://swapi.dev/api/starships") {
 }
 
 async function fetch_pilot(pilot) {
-    //! if (pilots[pilot]) return; // prev duplicate fetches - redundant due to Set
-    console.log(`fetching pilot: ${pilot}`)
+    // console.log(`fetching pilot: ${pilot}`)
     try {
         const response = await fetch(`https://swapi.dev/api/people/${pilot}`);
         pilots[pilot] = await response.json();
     } catch (err) {
-        console.error("pil-err- ", {response})
         console.error(`error fetching pilot ${pilot}: `, err)
     }
 }
 
-await fetch_starships();
-console.log("starships fetched")
 
-// get a list of all pilots then fetch in parallel
+console.log("Fetching Starships...")
+await fetch_starships();
+
+// get a list of all pilots then fetch in parallel - this is done to decrease the time needed to fetch starships
 const pilotNums = new Set();
 
 // gets the pilot num from the url
 const getPilotNum = url => url.match(/(\d+)/)[0];
 
-// I choose to not use {} when only a single line of code needed, imo it looks cleaner for short sections
-for (const ship of starships)
-    for (const pil_url of ship.pilots)
-         pilotNums.add(getPilotNum(pil_url));
+for (const ship of starships){
+    for (const pil_url of ship.pilots){
+        pilotNums.add(getPilotNum(pil_url))
+    }
+}
 
 
-
+console.log("Fetching Pilots... ")
 await Promise.all([...pilotNums].map(fetch_pilot))
 
-console.log("s len: ", starships.length)
-console.log("p len: ", Object.keys(pilots).length)
-// console.log("nums: ", pilotNums);
-// console.log(pilots['1'])
-// console.log(starships)
+// console.log("s len: ", starships.length)
+// console.log("p len: ", Object.keys(pilots).length)
+
+starships.forEach(ship=>{
+    console.log("\nStarship:", ship.name)
+    if (!ship.pilots.length){
+        return console.log("    No Pilots...");
+    }
+    for(const pilot of ship.pilots){
+        const pilNum = getPilotNum(pilot);
+        console.log("    ",pilots[pilNum]['name'])
+    }
+})
+
+// console.log(pilots)
+// console.log(starships[0].pilots)
